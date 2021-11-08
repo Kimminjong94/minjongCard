231e23dd8393e3d846f2c2689a62bb33cd347f6f
minjongkim <rlaalswhd200@naver.com>
2021-11-08 13:32:10 +0900

    Initial Commit

---
+++ minjongCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  minjongCard
+//
+//  Created by 김민종 on 2021/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ minjongCard/minjongCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  minjongCardApp.swift
+//  minjongCard
+//
+//  Created by 김민종 on 2021/11/08.
+//
+
+import SwiftUI
+
+@main
+struct minjongCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
